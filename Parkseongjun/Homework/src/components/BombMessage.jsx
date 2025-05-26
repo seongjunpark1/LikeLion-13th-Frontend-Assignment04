1d2fdd684d8bd648a0ec4e6e157240508adf648d
seongjunpark1 <pysida70@naver.com>
2025-05-26 19:41:35 +0900

    4주차 과제 수정

--- Parkseongjun/Homework/src/components/BombMessage.jsx
+++ Parkseongjun/Homework/src/components/BombMessage.jsx
@@ -4,15 +4,12 @@ import "./BombMessage.css"
 // 과제 조건 1번 충족
 function BombTimer() {
     const [second, setSecond] = useState(5);
-    const [effect, setEffect] = useState(false);
+    // const [effect, setEffect] = useState(false);
 
 
     // 과제 조건 3번 충족
     useEffect(() => {
-            if (second === 0) {
-                setEffect(true);
-                return;
-            } 
+            if (second === 0) return;
 
             const timer = setInterval(() => {
                 setSecond((prev) => prev - 1);
@@ -22,15 +19,15 @@ function BombTimer() {
         },[second]);
 
         return (
-            <div className="effect-container">
-                <h2>과연 뭐가 나올까?</h2>
-                <h2 className={effect ? "fighting" : "countDown"}>
-                    {/* 과제 조건 2번 충족 */}
-                    {effect ? "흐에!! 멋사 화이팅" : `${second}초 후에 폭파됩니다.`}
-                </h2>
+            <div>
+                {second > 0 && (
+                <div className="effect-container">
+                    <h2 className="fighting">흐에!! 멋사 화이팅</h2>
+                    <span>{second}초 뒤에 폭파됩니다!</span>
+                </div>
+                )}
             </div>
-        )
-        
+        )   
 }
 
 export default BombTimer;
